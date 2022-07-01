ca1b6f7c692c787b8c7832ee227648037078f8a0
NandeepRaju <nandi062@gmail.com>
2022-07-01 20:00:03 +0530

    Committing Qualitest code

---
+++ Qualitest/src/test/java/TestRunner/testRunner.java
@@ -0,0 +1,17 @@
+package TestRunner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(features="src/test/resources/Features/cartItems.feature",
+glue={"StepDefinition"}, monochrome = true, plugin = {"pretty",
+"html:target/HtmlReports/report.html", "json:target/JsonReports/report.json",
+"junit:target/JUnitReports/report.xml"})
+
+public class testRunner {
+
+}
